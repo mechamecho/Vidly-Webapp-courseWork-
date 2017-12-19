ce93fafda2bfec4851c7802cef4a024aa5dcd813
Livepurposefully <nafissa_92@hotmail.com>
2017-12-18 22:44:03 -0500

    Razor Syntax and ViewModels

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -10,13 +12,20 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {   
             var movie = new Movie() {Name = "Shrek!"};
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"},
+                new Customer{Name="Customer 3"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
         }
 
-        [Route("movies/released/{year:regex(//d{4})/month:regex(//d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year+ "/" + month);
-        }
     }
 }

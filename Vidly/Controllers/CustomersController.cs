04cb6b90b481256dab762e70629889b46769b98a
Livepurposefully <nafissa_92@hotmail.com>
2017-12-22 17:43:02 -0500

    Add AntiForgery token validation

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -46,6 +46,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)

7aac8e99868ed41803314f839902f33fd21808d8
Livepurposefully <nafissa_92@hotmail.com>
2017-12-24 17:15:26 -0500

    Add Datatable for Movies, and use Api to render the data, and create the view on the client side

--- Vidly/Dtos/MovieDto.cs
+++ Vidly/Dtos/MovieDto.cs
@@ -15,10 +15,10 @@ namespace Vidly.Dtos
         [StringLength(255)]
         public string Name { get; set; }
 
-        public Genre Genre { get; set; }
-
         [Required]
         public byte GenreId { get; set; }
+
+        public GenreDto Genre { get; set; }
 
 
         public DateTime DateAdded { get; set; }

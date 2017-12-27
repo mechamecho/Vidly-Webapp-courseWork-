2c697c17161e8355eb52fdffe19d63800c160970
Livepurposefully <nafissa_92@hotmail.com>
2017-12-26 20:22:14 -0500

    Cache Index action in HomeController, and Disable Caching in About action in HomeController

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -1,15 +1,24 @@
 ﻿using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Vidly.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        //Duration is for time in seconds not ms
+        //Location choose client if it is for a specific reason,
+        //but server if it is for all users
+        //VaryByParam is to have different cached pages depending on params * for all params
+
+        [OutputCache(Duration=50, Location = OutputCacheLocation.Server, VaryByParam = "*")]
         public ActionResult Index()
         {
             return View();
         }
 
+        //To disable caching for an action
+        [OutputCache (Duration = 0, VaryByParam = "*", NoStore = true)]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";

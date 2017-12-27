2e8f14f7093456efe351affdcaa6c56466dd25d1
Livepurposefully <nafissa_92@hotmail.com>
2017-12-27 01:51:37 -0500

    Validate edge cases in NewRentalsController for CreateNewRentals action. Defensive approach for public API.

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -20,11 +20,25 @@ namespace Vidly.Controllers.Api
             var customer = _context.Customers.Single(
                 c => c.Id == newRental.CustomerId);
 
+            if (newRental.MovieIds.Count == 0)
+                return BadRequest("No movie Ids have been given");
+
+            if (customer == null)
+                return BadRequest("Customer Id is not Valid.");
+
             var movies = _context.Movies.Where(
-                m => newRental.MovieIds.Contains(m.Id));
+                m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            if (movies.Count != newRental.MovieIds.Count)
+                return BadRequest("One or MovieIds are invalid.");
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("This movie is currently not available");
+
+                movie.NumberAvailable--;
+
                 var rental = new Rental
                 {
                     Customer = customer,
